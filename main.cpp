9034fb3298a5f75944395fb3d7f8e57b42ad2879
pd0220 <david.pesznyak@gmail.com>
2020-04-01 21:19:08 +0200

    hopefully working fit and fit error estimation

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// chi squared method implementation for numerical curve fitting
+// chi squared method implementation for numerical curve fitting with estimating error via jackknife samples
 // icluding useful libraries and/or headers
 #include <iostream>
 #include <string>
@@ -6,22 +6,41 @@
 #include <vector>
 #include <fstream>
 #include <math.h>
+#include <numeric>
 // Eigen library for linear algebra
 #include <Eigen/Dense>
 
 // ------------------------------------------------------------------------------------------------------------
 
 // read given file
+// expected structure: x | y | y_err | y_jck...
 Eigen::MatrixXd readFile(std::string fileName)
 {
+    // start reading
+    std::ifstream fileToRead;
+    fileToRead.open(fileName);
+
+    // determine number of columns (3 + N_jck)
+    std::string firstLine;
+    std::getline(fileToRead, firstLine);
+    std::stringstream firstLineStream(firstLine);
+    // number of columns in given file
+    int numOfCols = 0;
+    std::string temp;
+    // count number of writes to temporary string container
+    while (firstLineStream >> temp)
+    {
+        numOfCols++;
+    }
+    fileToRead.close();
+
     // string for lines
     std::string line;
 
     // data structure to store data
-    Eigen::MatrixXd DataMat(0, 3);
+    Eigen::MatrixXd DataMat(0, numOfCols);
 
-    // start reading
-    std::ifstream fileToRead;
+    // reopen file
     fileToRead.open(fileName);
     // check if open
     if (fileToRead.is_open())
@@ -32,8 +51,11 @@ Eigen::MatrixXd readFile(std::string fileName)
         {
             // using stringstream to write matrix
             std::stringstream dataStream(line);
-            DataMat.conservativeResize(i + 1, 3);
-            dataStream >> DataMat(i, 0) >> DataMat(i, 1) >> DataMat(i, 2);
+            DataMat.conservativeResize(i + 1, numOfCols);
+            for (int j = 0; j < numOfCols; j++)
+            {
+                dataStream >> DataMat(i, j);
+            }
             i++;
         }
         // close file
@@ -52,23 +74,68 @@ Eigen::MatrixXd readFile(std::string fileName)
 
 // ------------------------------------------------------------------------------------------------------------
 
+// RHS vector for linear equation system
+Eigen::VectorXd RHS(int length, Eigen::VectorXd yData, Eigen::VectorXd errData)
+{
+    Eigen::VectorXd bVec(length);
+
+    for (int i = 0; i < length; i++)
+    {
+        bVec(i) = yData(i) / errData(i);
+    }
+
+    return bVec;
+}
+
+// ------------------------------------------------------------------------------------------------------------
+
+Eigen::VectorXd JackknifeErrorEstimation(Eigen::VectorXd coeffs, std::vector<Eigen::VectorXd> JCKCoeffs)
+{
+    int length = coeffs.size();
+    int N = static_cast<int>(JCKCoeffs.size());
+    double preFactor = (double)(N - 1) / N;
+    auto sq = [](double x) { return x * x; };
+
+    Eigen::VectorXd sigmaSqVec(length);
+
+    for (int i = 0; i < length; i++)
+    {
+        sigmaSqVec(i) = 0;
+        for (int j = 0; j < N; j++)
+        {
+            sigmaSqVec(i) += sq(coeffs(i) - JCKCoeffs[j](i));
+        }
+        sigmaSqVec(i) = std::sqrt(preFactor * sigmaSqVec(i));
+    }
+
+    return sigmaSqVec;
+}
+
+// ------------------------------------------------------------------------------------------------------------
+
 // main function
 // argv[1] is datafile to fit
 //       1st col --> some physical quantity (x)
 //       2nd col --> data (y)
 //       3rd col --> err (sigma)
-// argv[2] is highest degree of basis functions
+//  rest of cols --> Jackknife samples (y_jck)
+// rest of argv --> if given degree should be fitted
 int main(int argc, char **argv)
 {
     // file name
     std::string fileName = "None";
-    // degree of highest basis function
-    int deg = -1;
     // check for arguments
-    if (argc > 2)
+    fileName = argv[1];
+    // container for polynomial degress
+    std::vector<bool> degContainer(argc - 2, 0);
+    int numOfBasis = 0;
+    for (int i = 0; i < argc - 2; i++)
     {
-        fileName = argv[1];
-        deg = std::stoi(argv[2]);
+        degContainer[i] = std::stoi(argv[i + 2]);
+        if (degContainer[i])
+        {
+            numOfBasis++;
+        }
     }
 
     // error check
@@ -77,9 +144,9 @@ int main(int argc, char **argv)
         std::cout << "No file was given, or the file dose not exist or unavailable." << std::endl;
         std::exit(-1);
     }
-    if (deg < 0)
+    if (argc < 3)
     {
-        std::cout << "No degree of basis functions was given." << std::endl;
+        std::cout << "No polynomial degrees were given." << std::endl;
         std::exit(-1);
     }
 
@@ -89,41 +156,65 @@ int main(int argc, char **argv)
     // "length" of data
     int length = rawDataMat.rows();
 
-    // containers for x, y, err
+    // number if jackknife samples
+    int numJCK = rawDataMat.cols() - 3;
+
+    // containers for x, y, err and y_jck
     Eigen::VectorXd xData(length);
     Eigen::VectorXd yData(length);
     Eigen::VectorXd errData(length);
+    std::vector<Eigen::VectorXd> jackknifeContainer(numJCK, Eigen::VectorXd(length));
 
     // fill containers
     for (int i = 0; i < length; i++)
     {
         xData(i) = rawDataMat(i, 0), yData(i) = rawDataMat(i, 1), errData(i) = rawDataMat(i, 2);
+        for (int j = 0; j < numJCK; j++)
+        {
+            jackknifeContainer[j](i) = rawDataMat(i, 3 + j);
+        }
     }
 
     // matrix for linear equation system
-    Eigen::MatrixXd MMat(length, deg + 1);
-
+    Eigen::MatrixXd MMat(length, numOfBasis);
     for (int i = 0; i < length; i++)
     {
-        for (int j = 0; j <= deg; j++)
+        int basis = 0;
+        for (int j = 0; j < static_cast<int>(degContainer.size()); j++)
         {
-            // basis function used on x divided by err
-            MMat(i, j) = std::pow(xData(i), j) / (double)errData(i);
+            if (degContainer[j] == true)
+            {
+                // basis function used on x divided by err
+                MMat(i, basis) = std::pow(xData(i), j) / (double)errData(i);
+                basis++;
+            }
         }
     }
 
     // transposing the previous matrix
     Eigen::MatrixXd MMatTranspose = MMat.transpose();
+    // matrix for the equation system
+    Eigen::MatrixXd MTxM = MMatTranspose * MMat;
 
     // RHS vector for linear equation system
-    Eigen::VectorXd bVec(length);
+    Eigen::VectorXd bVec = RHS(length, yData, errData);
 
-    for (int i = 0; i < length; i++)
+    // solving the linear equqation system for fitted coefficients
+    Eigen::VectorXd coeffVector = (MTxM).fullPivLu().solve(MMatTranspose * bVec);
+
+    // coefficients for jackknife samples
+    std::vector<Eigen::VectorXd> coeffJackknife(numJCK, Eigen::VectorXd(numOfBasis));
+
+    // making fit for jackknife samples
+    for (int i = 0; i < numJCK; i++)
     {
-        // data point y divided by err
-        bVec(i) = yData(i) / errData(i);
+        coeffJackknife[i] = (MTxM).fullPivLu().solve(MMatTranspose * RHS(length, jackknifeContainer[i], errData));
     }
 
-    // solving the linear equqation system and write fitted coefficients to screen
-    std::cout << (MMatTranspose * MMat).fullPivLu().solve(MMatTranspose * bVec) << std::endl;
+    // error estimation
+    Eigen::VectorXd errorVec = JackknifeErrorEstimation(coeffVector, coeffJackknife);
+
+    // to screen
+    std::cout << coeffVector << std::endl;
+    std::cout << errorVec << std::endl;
 }
